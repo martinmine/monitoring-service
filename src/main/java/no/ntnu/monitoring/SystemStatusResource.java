de28d5a40d1242286ce0d20c68cc7f9773d74e00
martinmine <martin_mine@hotmail.com>
2017-10-03 12:21:26 +0200

    Cleanup

--- src/main/java/no/ntnu/monitoring/SystemStatusResource.java
+++ src/main/java/no/ntnu/monitoring/SystemStatusResource.java
@@ -7,7 +7,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.io.IOException;
 import java.lang.management.ManagementFactory;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -18,26 +17,6 @@ public class SystemStatusResource {
 
     private static final Logger LOGGER = Logger.getLogger(SystemStatusResource.class.getName());
 
-    @GET
-    public Response getSystemStatus() {
-        return Response.ok("Hello world").build();
-
-
-
-        /*
-        • Bytes sent/received
-        • Packets sent/received
-        • Power usage (watt)
-        • Requests sent
-        • Requests received
-         */
-    }
-
-    /**
-     *
-     *
-     * @return
-     */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response getCPUUsage() {
